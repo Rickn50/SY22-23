dcb97ece15ab398e08e54e607c0242649c780fc2
THE_LAD <rmmen232@learner.dvusd.org>
2022-08-17 14:06:19 -0700

    Wednesday Update

---
+++ VB/WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,268 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.LuffyButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.namelabel = New System.Windows.Forms.Label()
+        Me.legslabel = New System.Windows.Forms.Label()
+        Me.heightlabel = New System.Windows.Forms.Label()
+        Me.bouncemanluffyButton = New System.Windows.Forms.Button()
+        Me.TankmanButton = New System.Windows.Forms.Button()
+        Me.Gear5Button = New System.Windows.Forms.Button()
+        Me.GearSecondButton = New System.Windows.Forms.Button()
+        Me.SecondGearPictureBox = New System.Windows.Forms.PictureBox()
+        Me.Gear5PictureBox = New System.Windows.Forms.PictureBox()
+        Me.TankManPictureBox = New System.Windows.Forms.PictureBox()
+        Me.BounceManPictureBox = New System.Windows.Forms.PictureBox()
+        Me.luffyPictureBox = New System.Windows.Forms.PictureBox()
+        Me.displaypicturebox = New System.Windows.Forms.PictureBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.SecondGearPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Gear5PictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TankManPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BounceManPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.luffyPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.displaypicturebox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'LuffyButton
+        '
+        Me.LuffyButton.Location = New System.Drawing.Point(2, 201)
+        Me.LuffyButton.Name = "LuffyButton"
+        Me.LuffyButton.Size = New System.Drawing.Size(123, 23)
+        Me.LuffyButton.TabIndex = 9
+        Me.LuffyButton.Text = "Snake Man Luffy"
+        Me.LuffyButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(215, -1)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 16)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(216, 33)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(96, 16)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Best Used For:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(216, 68)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 16)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "Height"
+        '
+        'namelabel
+        '
+        Me.namelabel.AutoSize = True
+        Me.namelabel.Location = New System.Drawing.Point(336, -1)
+        Me.namelabel.Name = "namelabel"
+        Me.namelabel.Size = New System.Drawing.Size(0, 16)
+        Me.namelabel.TabIndex = 15
+        '
+        'legslabel
+        '
+        Me.legslabel.AutoSize = True
+        Me.legslabel.Location = New System.Drawing.Point(336, 33)
+        Me.legslabel.Name = "legslabel"
+        Me.legslabel.Size = New System.Drawing.Size(0, 16)
+        Me.legslabel.TabIndex = 16
+        '
+        'heightlabel
+        '
+        Me.heightlabel.AutoSize = True
+        Me.heightlabel.Location = New System.Drawing.Point(336, 68)
+        Me.heightlabel.Name = "heightlabel"
+        Me.heightlabel.Size = New System.Drawing.Size(0, 16)
+        Me.heightlabel.TabIndex = 17
+        '
+        'bouncemanluffyButton
+        '
+        Me.bouncemanluffyButton.Location = New System.Drawing.Point(-1, 155)
+        Me.bouncemanluffyButton.Name = "bouncemanluffyButton"
+        Me.bouncemanluffyButton.Size = New System.Drawing.Size(126, 23)
+        Me.bouncemanluffyButton.TabIndex = 18
+        Me.bouncemanluffyButton.Text = "BounceMan Luffy"
+        Me.bouncemanluffyButton.UseVisualStyleBackColor = True
+        '
+        'TankmanButton
+        '
+        Me.TankmanButton.Location = New System.Drawing.Point(-1, 247)
+        Me.TankmanButton.Name = "TankmanButton"
+        Me.TankmanButton.Size = New System.Drawing.Size(123, 23)
+        Me.TankmanButton.TabIndex = 20
+        Me.TankmanButton.Text = "TankMan Luffy"
+        Me.TankmanButton.UseVisualStyleBackColor = True
+        '
+        'Gear5Button
+        '
+        Me.Gear5Button.Location = New System.Drawing.Point(-1, 289)
+        Me.Gear5Button.Name = "Gear5Button"
+        Me.Gear5Button.Size = New System.Drawing.Size(110, 23)
+        Me.Gear5Button.TabIndex = 22
+        Me.Gear5Button.Text = "Gear 5 Luffy"
+        Me.Gear5Button.UseVisualStyleBackColor = True
+        '
+        'GearSecondButton
+        '
+        Me.GearSecondButton.Location = New System.Drawing.Point(-1, 111)
+        Me.GearSecondButton.Name = "GearSecondButton"
+        Me.GearSecondButton.Size = New System.Drawing.Size(131, 23)
+        Me.GearSecondButton.TabIndex = 24
+        Me.GearSecondButton.Text = "Gear Second"
+        Me.GearSecondButton.UseVisualStyleBackColor = True
+        '
+        'SecondGearPictureBox
+        '
+        Me.SecondGearPictureBox.Image = Global.WindowsApp1.My.Resources.Resources.one_piece_luffy_anime_gear_1206013_1280x0
+        Me.SecondGearPictureBox.Location = New System.Drawing.Point(813, 230)
+        Me.SecondGearPictureBox.Name = "SecondGearPictureBox"
+        Me.SecondGearPictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.SecondGearPictureBox.TabIndex = 25
+        Me.SecondGearPictureBox.TabStop = False
+        Me.SecondGearPictureBox.Visible = False
+        '
+        'Gear5PictureBox
+        '
+        Me.Gear5PictureBox.Image = Global.WindowsApp1.My.Resources.Resources.monkey_d__luffy__gear_5____updated_by_b_a_i_o_r_e_t_t_o_df3gezw_fullview
+        Me.Gear5PictureBox.Location = New System.Drawing.Point(810, 174)
+        Me.Gear5PictureBox.Name = "Gear5PictureBox"
+        Me.Gear5PictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.Gear5PictureBox.TabIndex = 23
+        Me.Gear5PictureBox.TabStop = False
+        Me.Gear5PictureBox.Visible = False
+        '
+        'TankManPictureBox
+        '
+        Me.TankManPictureBox.Image = Global.WindowsApp1.My.Resources.Resources._render__luffy_gear_4__tank_man_by_hobbj_de2dc8l_fullview
+        Me.TankManPictureBox.Location = New System.Drawing.Point(813, 113)
+        Me.TankManPictureBox.Name = "TankManPictureBox"
+        Me.TankManPictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.TankManPictureBox.TabIndex = 21
+        Me.TankManPictureBox.TabStop = False
+        Me.TankManPictureBox.Visible = False
+        '
+        'BounceManPictureBox
+        '
+        Me.BounceManPictureBox.Image = Global.WindowsApp1.My.Resources.Resources.dbute9h_b3c71a28_b01b_45a2_bb1f_76c4f8aa0fe7
+        Me.BounceManPictureBox.Location = New System.Drawing.Point(810, 55)
+        Me.BounceManPictureBox.Name = "BounceManPictureBox"
+        Me.BounceManPictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.BounceManPictureBox.TabIndex = 19
+        Me.BounceManPictureBox.TabStop = False
+        Me.BounceManPictureBox.Visible = False
+        '
+        'luffyPictureBox
+        '
+        Me.luffyPictureBox.Image = Global.WindowsApp1.My.Resources.Resources.dddx41j_6a7314b3_18ee_403b_8524_3f1bc7da57f7
+        Me.luffyPictureBox.Location = New System.Drawing.Point(810, -1)
+        Me.luffyPictureBox.Name = "luffyPictureBox"
+        Me.luffyPictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.luffyPictureBox.TabIndex = 11
+        Me.luffyPictureBox.TabStop = False
+        Me.luffyPictureBox.Visible = False
+        '
+        'displaypicturebox
+        '
+        Me.displaypicturebox.Location = New System.Drawing.Point(-1, -1)
+        Me.displaypicturebox.Name = "displaypicturebox"
+        Me.displaypicturebox.Size = New System.Drawing.Size(196, 106)
+        Me.displaypicturebox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.displaypicturebox.TabIndex = 10
+        Me.displaypicturebox.TabStop = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(271, 102)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 16)
+        Me.Label4.TabIndex = 27
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(913, 486)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.SecondGearPictureBox)
+        Me.Controls.Add(Me.GearSecondButton)
+        Me.Controls.Add(Me.Gear5PictureBox)
+        Me.Controls.Add(Me.Gear5Button)
+        Me.Controls.Add(Me.TankManPictureBox)
+        Me.Controls.Add(Me.TankmanButton)
+        Me.Controls.Add(Me.BounceManPictureBox)
+        Me.Controls.Add(Me.bouncemanluffyButton)
+        Me.Controls.Add(Me.heightlabel)
+        Me.Controls.Add(Me.legslabel)
+        Me.Controls.Add(Me.namelabel)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.luffyPictureBox)
+        Me.Controls.Add(Me.displaypicturebox)
+        Me.Controls.Add(Me.LuffyButton)
+        Me.Name = "Form1"
+        Me.Text = "LuffyDex"
+        CType(Me.SecondGearPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Gear5PictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TankManPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BounceManPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.luffyPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.displaypicturebox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LuffyButton As Button
+    Friend WithEvents displaypicturebox As PictureBox
+    Friend WithEvents luffyPictureBox As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents namelabel As Label
+    Friend WithEvents legslabel As Label
+    Friend WithEvents heightlabel As Label
+    Friend WithEvents BounceManPictureBox As PictureBox
+    Friend WithEvents bouncemanluffyButton As Button
+    Friend WithEvents TankManPictureBox As PictureBox
+    Friend WithEvents TankmanButton As Button
+    Friend WithEvents Gear5Button As Button
+    Friend WithEvents Gear5PictureBox As PictureBox
+    Friend WithEvents GearSecondButton As Button
+    Friend WithEvents SecondGearPictureBox As PictureBox
+    Friend WithEvents Label4 As Label
+End Class
---
+++ VB/WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form1
+    Private Sub LuffyButton_Click(sender As Object, e As EventArgs) Handles LuffyButton.Click
+        displaypicturebox.Image = luffyPictureBox.Image
+        namelabel.Text = "SnakeMan Luffy"
+        heightlabel.Text = "9 feet"
+        legslabel.Text = "Speed"
+    End Sub
+
+    Private Sub bouncemanluffyButton_Click(sender As Object, e As EventArgs) Handles bouncemanluffyButton.Click
+        displaypicturebox.Image = BounceManPictureBox.Image
+        namelabel.Text = "Bounce Man Luffy"
+        heightlabel.Text = "13 feet"
+        legslabel.Text = "Strength"
+    End Sub
+
+    Private Sub TankmanButton_Click(sender As Object, e As EventArgs) Handles TankmanButton.Click
+        displaypicturebox.Image = TankManPictureBox.Image
+        namelabel.Text = "Tank Man Luffy"
+        heightlabel.Text = "22-24 feet"
+        legslabel.Text = "Defense"
+    End Sub
+
+    Private Sub Gear5Button_Click(sender As Object, e As EventArgs) Handles Gear5Button.Click
+        displaypicturebox.Image = Gear5PictureBox.Image
+        namelabel.Text = "Gear 5 Luffy"
+        heightlabel.Text = "5 feet"
+        legslabel.Text = "Crazy attacks"
+    End Sub
+
+    Private Sub GearSecondButton_Click(sender As Object, e As EventArgs) Handles GearSecondButton.Click
+        displaypicturebox.Image = SecondGearPictureBox.Image
+        namelabel.Text = "Second Gear Luffy"
+        heightlabel.Text = "5 feet"
+        legslabel.Text = "Small power up"
+    End Sub
+End Class
