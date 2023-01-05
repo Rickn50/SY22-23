35a23e0d4491a6706e3bc8fda9d252a813fde759
THE_LAD <rmmen232@learner.dvusd.org>
2023-01-05 14:07:25 -0700

    ADDED MENU

--- VB/WpfApp1/WpfApp1/MainWindow.xaml.vb
+++ VB/WpfApp1/WpfApp1/MainWindow.xaml.vb
@@ -1,7 +1,31 @@
 ﻿Imports System.Net.Security
 
 Class MainWindow
-    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
-        TextBox1.Text = "Hello World"
+    Private Sub Bold_Checked(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        textBox1.FontWeight = FontWeights.Bold
+    End Sub
+
+    Private Sub Bold_Unchecked(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        textBox1.FontWeight = FontWeights.Normal
+    End Sub
+
+    Private Sub Italic_Checked(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        textBox1.FontStyle = FontStyles.Italic
+    End Sub
+
+    Private Sub Italic_Unchecked(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        textBox1.FontStyle = FontStyles.Normal
+    End Sub
+
+    Private Sub IncreaseFont_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        If textBox1.FontSize < 18 Then
+            textBox1.FontSize += 2
+        End If
+    End Sub
+
+    Private Sub DecreaseFont_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        If textBox1.FontSize > 10 Then
+            textBox1.FontSize -= 2
+        End If
     End Sub
 End Class
